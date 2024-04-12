1a4fd0a609a90c73b25affb6ca0c4ad7ada35194
5tr1ker <tjseocld@naver.com>
2024-04-13 03:30:05 +0900

    docs: Swagger 설명문 추가
    docs: Swagger 설명문 추가

--- src/main/java/com/team/saver/partner/request/controller/PartnerRequestController.java
+++ src/main/java/com/team/saver/partner/request/controller/PartnerRequestController.java
@@ -5,6 +5,7 @@ import com.team.saver.partner.request.dto.NewPartnerRequest;
 import com.team.saver.partner.request.dto.PartnerRequestResponse;
 import com.team.saver.partner.request.entity.PartnerRequest;
 import com.team.saver.partner.request.service.PartnerRequestService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +23,7 @@ public class PartnerRequestController {
     private final PartnerRequestService partnerRequestService;
 
     @PostMapping
+    @Operation(summary = "새로운 파트너십 요청 API [ 사용자 인증 정보 필요 ]")
     public ResponseEntity requestNewPartner(@RequestBody NewPartnerRequest request
             , @AuthenticationPrincipal UserDetails userDetails) {
         partnerRequestService.requestNewPartner(request, userDetails);
@@ -30,6 +32,7 @@ public class PartnerRequestController {
     }
 
     @GetMapping
+    @Operation(summary = "요청된 모든 파트너십 데이터 가져오기")
     public ResponseEntity findAllEntity() {
         List<PartnerRequestResponse> result = partnerRequestService.findAllEntity();
 
