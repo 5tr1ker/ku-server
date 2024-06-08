24421a9a9c14d696dea2647433050d81a0742060
5tr1ker <tjseocld@naver.com>
2024-06-09 00:51:38 +0900

    feat: 로그인 시 OAuth2.0 서버 구별
    feat: 로그인 시 OAuth2.0 서버 구별

--- src/main/java/com/team/saver/common/dto/ErrorMessage.java
+++ src/main/java/com/team/saver/common/dto/ErrorMessage.java
@@ -15,6 +15,7 @@ public enum ErrorMessage {
     SMTP_SERVER_ERROR("메일 서버에서 오류가 발생했습니다."),
     ALREADY_FAVORITE_MARKET("이미 관심 목록에 추가된 가게입니다."),
     UNKNOWN_OAUTH_TYPE("알 수 없는 OAuth Type 입니다."),
+    ANOTHER_OAUTH_SERVER("%s 계정으로 가입된 계정입니다."),
     NOT_FOUND_PARTNER_REQUEST("파트너 요청 페이지를 찾을 수 없습니다."),
     NOT_FOUND_MARKET("해당 매장을 찾을 수 없습니다."),
     ONLY_DELETE_WRITER("작성자만 삭제할 수 있습니다."),
