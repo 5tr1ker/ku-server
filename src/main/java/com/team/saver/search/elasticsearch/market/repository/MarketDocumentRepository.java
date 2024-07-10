676fa4eb8bcf8d8c736f8d1d7fbca039a7d63093
5tr1ker <tjseocld@naver.com>
2024-07-10 22:38:04 +0900

    feat: Pageable 기능 구현
    feat: Pageable 기능 구현

--- src/main/java/com/team/saver/search/elasticsearch/market/repository/MarketDocumentRepository.java
+++ src/main/java/com/team/saver/search/elasticsearch/market/repository/MarketDocumentRepository.java
@@ -1,12 +1,14 @@
 package com.team.saver.search.elasticsearch.market.repository;
 
 import com.team.saver.search.elasticsearch.market.document.MarketDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
 
 public interface MarketDocumentRepository extends ElasticsearchRepository<MarketDocument, Long> {
 
-    List<MarketDocument> findByMarketName(String marketName);
+    Page<MarketDocument> findByMarketName(String marketName, Pageable pageable);
 
 }
--- src/main/java/com/team/saver/search/elasticsearch/market/service/MarketDocumentService.java
+++ src/main/java/com/team/saver/search/elasticsearch/market/service/MarketDocumentService.java
@@ -3,6 +3,9 @@ package com.team.saver.search.elasticsearch.market.service;
 import com.team.saver.search.elasticsearch.market.document.MarketDocument;
 import com.team.saver.search.elasticsearch.market.repository.MarketDocumentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,8 +16,10 @@ public class MarketDocumentService {
 
     private final MarketDocumentRepository marketDocumentRepository;
 
-    public List<MarketDocument> findByMarketName(String marketName) {
-        return marketDocumentRepository.findByMarketName(marketName);
+    public List<MarketDocument> findByMarketName(String marketName, Pageable pageable) {
+        Page<MarketDocument> result = marketDocumentRepository.findByMarketName(marketName, pageable);
+
+        return result.getContent();
     }
 
 }
