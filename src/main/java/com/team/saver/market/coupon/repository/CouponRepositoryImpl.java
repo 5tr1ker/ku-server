7beb1954ceb6f98c7e7f1114b8da2903686e8b68
5tr1ker <tjseocld@naver.com>
2024-08-03 17:29:26 +0900

    feat: 쿠폰 노출 우선순위 변경
    feat: 쿠폰 노출 우선순위 변경

--- src/main/java/com/team/saver/market/coupon/repository/CouponRepositoryImpl.java
+++ src/main/java/com/team/saver/market/coupon/repository/CouponRepositoryImpl.java
@@ -113,7 +113,7 @@ public class CouponRepositoryImpl implements CustomCouponRepository {
                 .innerJoin(downloadCoupon.coupon, coupon)
                 .innerJoin(downloadCoupon.account, account).on(account.email.eq(userEmail))
                 .innerJoin(downloadCoupon.market, market)
-                .orderBy(coupon.priority.asc())
+                .orderBy(coupon.priority.desc())
                 .fetch();
     }
 
@@ -154,7 +154,7 @@ public class CouponRepositoryImpl implements CustomCouponRepository {
                 .innerJoin(downloadCoupon.account, account).on(account.email.eq(email))
                 .innerJoin(downloadCoupon.coupon, coupon).on(coupon.conditionToUseAmount.loe(orderPrice))
                 .innerJoin(coupon.market, market).on(market.marketId.eq(marketId).or(market.eventCouponMarket.eq(true)))
-                .orderBy(coupon.priority.asc())
+                .orderBy(coupon.priority.desc())
                 .where(downloadCoupon.isUsage.eq(false))
                 .fetch();
     }
