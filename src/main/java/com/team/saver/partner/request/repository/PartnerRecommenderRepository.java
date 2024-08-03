625d0cac7116acfbbc0e15ed1197bcda7be29265
5tr1ker <tjseocld@naver.com>
2024-08-03 23:09:44 +0900

    feat: 추천 제거 API 구현
    feat: 추천 제거 API 구현

---
+++ src/main/java/com/team/saver/partner/request/repository/PartnerRecommenderRepository.java
@@ -0,0 +1,8 @@
+package com.team.saver.partner.request.repository;
+
+import com.team.saver.partner.request.entity.PartnerRecommender;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartnerRecommenderRepository extends JpaRepository<PartnerRecommender, Long> {
+
+}
